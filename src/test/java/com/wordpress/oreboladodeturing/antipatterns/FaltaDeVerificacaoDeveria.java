cc4244eeed3c45266d3d0e9ce024a6b4737c7107
Felipe Pierin <felipe.pierin@gmail.com>
2012-04-19 17:09:19 -0300

    Explicacao de antipattern de teste

--- src/test/java/com/wordpress/oreboladodeturing/antipatterns/FaltaDeVerificacaoDeveria.java
+++ src/test/java/com/wordpress/oreboladodeturing/antipatterns/FaltaDeVerificacaoDeveria.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 
 public class FaltaDeVerificacaoDeveria {
 
+	/*
+	 * Nesta situação o teste está vazio e portanto nada está sendo verificado.
+	 */
 	@Test
 	public void verificaroComportamentoXDeUmMetodoY(){
 		// TODO
 	}
 
+	/*
+	 * Nesta situação o teste está definido, mas não há verificações.
+	 */
 	@Test
 	public void verificaroComportamentoXDeUmMetodoZ(){
 		new FaltaDeVerificacao().metodoZ();
