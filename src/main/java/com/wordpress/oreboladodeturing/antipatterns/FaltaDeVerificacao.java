1ccf16263dcb4923f5eb9baddfb93a90def83835
Felipe Pierin <felipe.pierin@gmail.com>
2012-04-19 17:05:19 -0300

    inclusao do primeiro exemplo de antipattern e configuracao do maven no eclipse

---
+++ src/main/java/com/wordpress/oreboladodeturing/antipatterns/FaltaDeVerificacao.java
@@ -0,0 +1,9 @@
+package com.wordpress.oreboladodeturing.antipatterns;
+
+public class FaltaDeVerificacao {
+
+	public void metodoZ() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/test/java/com/wordpress/oreboladodeturing/antipatterns/FaltaDeVerificacaoDeveria.java
@@ -0,0 +1,17 @@
+package com.wordpress.oreboladodeturing.antipatterns;
+
+import org.junit.Test;
+
+public class FaltaDeVerificacaoDeveria {
+
+	@Test
+	public void verificaroComportamentoXDeUmMetodoY(){
+		// TODO
+	}
+
+	@Test
+	public void verificaroComportamentoXDeUmMetodoZ(){
+		new FaltaDeVerificacao().metodoZ();
+	}
+
+}
